9a0a46016439537fb0dfa5928651c68a728fe0a4
RoarBoil <boilroar@gmail.com>
2020-06-25 23:24:53 +0800

    跨平台问题修复

---
+++ 包亦航2018011890/操作系统第一次作业：多缓冲区，水果，管程/Example_Program/WindowsVersion/Reader_Writer.py
@@ -0,0 +1,99 @@
+# -*- coding:utf-8 -*-
+"""
+    Spyder Editor
+    
+    Author: 
+        YihangBao
+        2018011890
+        
+    Purpose: 
+        Example of reader_writer
+    
+    Introduction:
+        A class for simulating Reader_Writer problem
+        
+    Special Tips:
+        a fasta file is needed for using this(an example fasta file has been offered in the datas dic)
+        
+    Created: 27/5/2020
+    
+    Last modified: 27/5/2020
+"""
+from datas.SimpleFastaDealer import SimpleFastaDealer
+import multiprocessing
+import random
+import time
+import os
+
+    
+
+    
+#-------------------------------------------------------------------------------------------------------------------
+def writer(queue,lock):
+    '''
+    Introduction
+    ------------
+    writer, it's a decorative class that produce one fasta sequence from the fasta file
+    
+    
+    Parameters
+    ----------
+    gener: the generator of fasta seq, details please look at SimpleFastaDealer.py
+    queue: the buffer queue
+    lock: this lock is not for buffer mutual exclusion as queue has its own way to do that,
+    here it's used to avoid messy print
+    ----------
+    '''
+    @SimpleFastaDealer('datas/TestFasta.fasta')
+    def run_p(gener):
+        for seq in gener:
+            time.sleep(random.random()*2)   #sleep for better impressions
+            lock.acquire()
+            print('Producing.........')
+            lock.release()
+            queue.put(seq)
+    run_p()
+
+#-------------------------------------------------------------------------------------------------------------------
+def read_and_print(queue,lock):
+    '''
+    Introduction
+    ------------
+    read_and_print, multi consumer may run at the same time 
+    
+    Parameters
+    ----------
+    see above
+    ----------
+    '''
+    counter = 0 #if counter is more than 10 than we regard the producer has rest
+    while True:
+        time.sleep(random.random()*10)
+        lock.acquire()
+        if queue.empty():   #if noe resouce
+            counter += 1
+        else:
+            counter = 0
+            seq = queue.get()
+            print('PID: ' + str(os.getpid()) + ' read a new sequence:')
+            print(seq)
+        lock.release()
+        if counter == 10:   #producer has rest! so consumer rest too
+            print('everyting of PID ' + str(os.getpid()) + ' is done!')
+            break
+        
+if __name__ == '__main__':
+    consumer_num=6
+    queue_length=6
+    record = []
+    lock  = multiprocessing.Lock()    # To prevent messy print
+    queue = multiprocessing.Queue(queue_length)    #The buffer queue
+    process1 = multiprocessing.Process(target=writer,args=(queue,lock))  #single producer
+    process1.start()
+    for i in range(consumer_num):  #multi consumer
+        process2 = multiprocessing.Process(target=read_and_print,args=(queue,lock))
+        process2.start()
+        record.append(process2)    #record every process for closing them later
+    process1.join()
+    for i in record:
+        i.join()    #close them
