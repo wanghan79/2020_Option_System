f3d1c8bbffc45c8716473ae53a515ca295af4d96
AlexandraGty <52900628+AlexandraGty@users.noreply.github.com>
2020-06-26 23:30:48 +0800

    Create 作业2.py
    第一次提交
    采用python语言实现windows命令行调用；提示：采用Python内置工具包os.system

---
+++ 顾天媛2018010980/操作系统实验/作业2.py
@@ -0,0 +1,12 @@
+# !/usr/bin/python3
+# -*- coding: UTF-8 -*-
+"""
+  Author:  Ty.Gu
+  Purpose: random data set & MongoDB
+  Created: 24/6/2020
+"""
+# 作业2. 采用python语言实现windows命令行调用；提示：采用Python内置工具包os.system
+import os
+os.system('cd D:\\QQ & mkdir test' )
+a = os.system('python 作业1.py')
+print(a)
