68aa4bb1b894a0ad6545f4186216553bb034df53
baismbaism <52900687+baismbaism@users.noreply.github.com>
2020-06-26 10:44:05 +0800

    操作系统实验作业1

---
+++ 白书铭2018012260/操作系统实验/作业1/getOSInformation_first.py
@@ -0,0 +1,15 @@
+"""
+Author: ShuMing.Bai
+Purpose: First: Get operating system information using package platform
+Created: 6/24/2020
+"""
+import platform
+
+print("操作系统及版本信息:", platform.platform())
+print('获取系统版本号:', platform.version())
+print('获取系统名称:', platform.system())
+print('系统位数:', platform.architecture())
+print('计算机类型:', platform.machine())
+print('计算机名称:', platform.node())
+print('处理器类型:', platform.processor())
+print('计算机相关信息:', platform.uname())
