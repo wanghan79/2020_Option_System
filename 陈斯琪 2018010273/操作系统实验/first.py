2ac70c2f292388cbe9de966c473e7ab0538fabee
2018010273 陈斯琪 <52900595+Chen-sq@users.noreply.github.com>
2020-06-26 11:10:51 +0800

    操作系统第一次作业
    操作系统第一次作业

---
+++ 陈斯琪 2018010273/操作系统实验/first.py
@@ -0,0 +1,14 @@
+import platform
+
+def show(name, info):
+    print("{}:{}".format(name, info))
+
+
+show("操作系统及版本信息", platform.platform())
+show('获取系统名称', platform.system())
+show('获取系统版本号', platform.version())
+show('系统位数', platform.architecture())
+show('计算机名称', platform.node())
+show('计算机类型', platform.machine())
+show('处理器类型', platform.processor())
+show('计算机相关信息', platform.uname())
