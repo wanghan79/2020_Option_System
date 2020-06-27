724e065a6ef17d3425fb434bd5b01aedfc0ba791
2018012953 曹芮嘉 <63714372+caoruijia412@users.noreply.github.com>
2020-06-27 22:09:12 +0800

    Add files via upload

---
+++ 曹芮嘉 2018012953/操作系统实验1.py
@@ -0,0 +1,18 @@
+# !/usr/bin/python3
+"""
+  Author:  RuiJia. Cao
+  Purpose: Generate random data set by iterator.
+  Created: 27/6/2020
+"""
+import platform
+def showinfo(tips, result):
+    print("{}:{}".format(tips, result))
+
+showinfo("操作系统及版本信息", platform.platform())
+showinfo('获取系统版本号', platform.version())
+showinfo('获取系统名称', platform.system())
+showinfo('系统位数', platform.architecture())
+showinfo('计算机类型', platform.machine())
+showinfo('计算机名称', platform.node())
+showinfo('处理器类型', platform.processor())
+showinfo('计算机相关信息', platform.uname())
