1fe38fcdbb81624353abf12c2edeb255b1a0e164
dongyy458 <53136755+dongyy458@users.noreply.github.com>
2020-06-29 15:24:00 +0800

    第三次实验

---
+++ 董玉叶2018010987/操作系统实验作业/第三次实验/homework3.py
@@ -0,0 +1,39 @@
+﻿"""
+Author: YY.Dong
+Purpose: Creating multiple processes
+Created: 25/6/2020
+"""
+# -*- coding: utf-8 -*-
+import os
+from multiprocessing import Process
+import time
+#继承process类
+
+
+
+
+def fun(name):
+    print("2 子进程信息： pid=%s, ppid=%s" % (os.getpid(), os.getppid()))
+    print("hello " + name)
+
+
+def test():
+    print('ssss')
+
+
+if __name__ == "__main__":
+    print("1 主进程信息： pid=%s, ppid=%s" % (os.getpid(), os.getppid()))
+    ps = Process(target=fun, args=('dyy', ))
+    print("111 ##### ps pid: " + str(ps.pid) + ", ident:" + str(ps.ident))
+    print("3 进程信息： pid=%s, ppid=%s" % (os.getpid(), os.getppid()))
+    print(ps.is_alive())
+    ps.start()
+    print(ps.is_alive())
+    print("222 #### ps pid: " + str(ps.pid) + ", ident:" + str(ps.ident))
+    print("4 进程信息： pid=%s, ppid=%s" % (os.getpid(), os.getppid()))
+    ps.join()
+    print(ps.is_alive())
+    print("5 进程信息： pid=%s, ppid=%s" % (os.getpid(), os.getppid()))
+    ps.terminate()
+    print("6 进程信息： pid=%s, ppid=%s" % (os.getpid(), os.getppid()))
+
