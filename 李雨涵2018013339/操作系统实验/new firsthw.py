5872ff0901539be312d9d93806e0b233ef996531
李雨涵2018013339 <52900684+liyuhan2000522@users.noreply.github.com>
2020-07-01 14:21:53 +0800

    实验一

---
+++ 李雨涵2018013339/操作系统实验/new firsthw.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/7/1 14:02
+# @Author  : liyuhan
+# @File    : new firsthw.py
+
+import platform
+
+def OsInfo():
+    print('获取操作系统名称及版本号',platform.platform())
+    print('获取操作系统版本号',platform.version())
+    print('获取操作系统位数',platform.architecture())
+    print('计算机类型',platform.machine())
+    print('计算机网络名称',platform.node())
+    print('计算机处理器信息',platform.processor())
+    print('计算机操作系统类型',platform.system())
+    print('汇总信息',platform.uname())
+
+def PyInfo():
+    print('获取python构建号和日期',platform.python_build())
+    print('获取用于编译python的编译器',platform.python_compiler())
+    print('获取python实现SCM分支',platform.python_branch())
+    print('获取python实现的字符串',platform.python_implementation())
+    print('获取python版本',platform.python_version())
+    print('获取python实现SCM修订',platform.python_revision())
+    print('获取python实现的数组',platform.python_version_tuple())
+
+
+if __name__ == '__main__':
+    OsInfo()
+    PyInfo()
