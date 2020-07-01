d7f9c59d17457f7e9b995628695fb8842fc3ab13
wtc202 <63712867+wtc202@users.noreply.github.com>
2020-07-01 11:40:51 +0800

    Add files via upload

---
+++ 王添晨2018013132/实验3.py
@@ -0,0 +1,17 @@
+
+import os
+
+def get_path(path):
+    lists = []
+    list = os.listdir(path)
+    for i in range(0, len(list)):
+        new_path = os.path.join(path, list[i])
+        if os.path.isfile(new_path):
+            lists.append(new_path)
+        if os.path.isdir(new_path):
+            lists.extend(get_path(new_path))
+    return lists
+path = input('输入目标路径')
+lists = []
+lists = get_path(path)
+print(lists)
