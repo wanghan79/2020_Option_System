6c126e8c0157cdfa8d8d30f0bbdf1560500ccf08
xTNTxx <63635276+xTNTxx@users.noreply.github.com>
2020-07-03 19:24:33 +0800

    Add files via upload

---
+++ 奚腾霄2018013027/操作系统实验/实验一.py
@@ -0,0 +1,16 @@
+
+import platform
+
+print("操作系统版本信息：", platform.platform())
+print('系统名称        ：', platform.system())
+print('系统版本号      ：', platform.version())
+print('系统位数        ：', platform.architecture())
+print('计算机类型      ：', platform.machine())
+print('计算机名称      ：', platform.node())
+print('处理器类型      ：', platform.processor())
+print('计算机相关信息  ：', platform.uname())
+
+
+
+
+
---
+++ 奚腾霄2018013027/操作系统实验/实验二.py
@@ -0,0 +1,13 @@
+#2018013027
+# coding: utf-8
+
+# In[1]:
+
+
+
+import os
+cmd='echo "Hello world!"'
+os.system(cmd)
+
+
+
