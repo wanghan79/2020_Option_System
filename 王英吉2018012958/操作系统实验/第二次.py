62647d90d4e038a9372aa3fc91c937d1e82a584d
Q1229 <64361429+Q1229@users.noreply.github.com>
2020-07-03 09:12:52 +0800

    Add files via upload

---
+++ 王英吉2018012958/操作系统实验/第二次.py
@@ -0,0 +1,11 @@
+import os
+
+os.system("calc")
+os.system("cmd.exe")
+os.system("control")
+os.system("appwiz.cpl")
+os.system("certmgr.msc")
+os.system("cleanmgr")
+os.system("cmstp")
+order = input()
+os.system(order)
