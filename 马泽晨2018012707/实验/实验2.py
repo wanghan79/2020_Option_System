c54cc45f0ebfe5e449f61d112d0bc70062534cd2
mazc134 <49559427+mazc134@users.noreply.github.com>
2020-07-03 20:13:55 +0800

    Add files via upload

---
+++ 马泽晨2018012707/实验/实验2.py
@@ -0,0 +1,9 @@
+import os
+
+#os.system("calc")
+#os.system("appwiz.cpl")
+#os.system("certmgr.msc")
+#os.system("charmap")
+#os.system("chkdsk.exe")
+order = input('imput your order')
+os.system(order)
