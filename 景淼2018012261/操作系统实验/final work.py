340ca951387f7ffc3af0edc23152286b8f2c71cc
JingM124 <52900456+JingM124@users.noreply.github.com>
2020-07-04 18:57:56 +0800

    Create final work.py

---
+++ 景淼2018012261/操作系统实验/final work.py
@@ -0,0 +1,15 @@
+import os
+
+
+def traverse(f):
+    fs = os.listdir(f)
+    for f1 in fs:
+        tmp_path = os.path.join(f, f1)
+        if not os.path.isdir(tmp_path):
+            print('文件: %s' % tmp_path)
+        else:
+            print('文件夹：%s' % tmp_path)
+            traverse(tmp_path)
+
+
+traverse("D:\Python")
